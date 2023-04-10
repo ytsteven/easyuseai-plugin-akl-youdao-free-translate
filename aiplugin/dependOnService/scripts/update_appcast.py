3bfa60cb0f01d3333857f68b8eaac146995fed72
ytsteven <1647605516@qq.com>
2023-04-10 16:48:24 +0800

    init

--- aiplugin/dependOnService/scripts/update_appcast.py
+++ aiplugin/dependOnService/scripts/update_appcast.py
@@ -5,10 +5,10 @@ from pathlib import Path
 
 
 def update_appcast(message):
-    with open ("bobplugin/dependOnService/src/info.json", "r") as f:
+    with open ("aiplugin/dependOnService/src/info.json", "r") as f:
         info = json.load(f)
     version = info["version"]
-    release_file = Path(f"release/bob-plugin-akl-youdao-free-translate.bobplugin")
+    release_file = Path(f"release/bob-plugin-akl-youdao-free-translate.aiplugin")
     assert release_file.is_file(), "Release file not exist"
     with open(release_file, "rb") as f:
         c = f.read()
@@ -17,15 +17,15 @@ def update_appcast(message):
         "version": version,
         "desc": message,
         "sha256": file_hash,
-        "url": f"https://github.com/akl7777777/bob-plugin-akl-youdao-free-translate/releases/download/v{version}/bob-plugin-akl-youdao-free-translate_v{version}.bobplugin",
+        "url": f"https://github.com/ytsteven/easyuseai-plugin-akl-youdao-free-translate/releases/download/v{version}/bob-plugin-akl-youdao-free-translate_v{version}.aiplugin",
         "minBobVersion": "0.5.0"
     }
-    appcast_file = Path("bobplugin/dependOnService/appcast.json")
+    appcast_file = Path("aiplugin/dependOnService/appcast.json")
     if appcast_file.is_file():
         with open(appcast_file, "r") as f:
             appcast = json.load(f)
     else:
-        appcast = dict(identifier="com.akl.bob-plugin-akl-youdao-free-translate", versions=[])
+        appcast = dict(identifier="com.akl.easyuseai-plugin-akl-youdao-free-translate", versions=[])
     appcast["versions"].insert(0, version_info)
     with open(appcast_file, "w") as f:
         json.dump(appcast, f, ensure_ascii=False, indent=2)
